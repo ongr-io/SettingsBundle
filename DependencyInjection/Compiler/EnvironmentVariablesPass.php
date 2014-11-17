9a34c4aabd317e1e770d58abbdabdb5e9c30c7df
Gediminas Šaltenis <gediminas.saltenis@ongr.io>
2014-11-17 13:15:43 +0200

    Forgot some files

---
+++ DependencyInjection/Compiler/EnvironmentVariablesPass.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\AdminBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+/**
+ * Environment variables pass, overrides default variables with environment ones.
+ */
+class EnvironmentVariablesPass implements CompilerPassInterface
+{
+    /**
+     * Finds environment variables prefixed with FOXX__ and changes default ones.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        foreach ($_SERVER as $key => $value) {
+            if (0 === strpos($key, 'ongr__')) {
+                $param = strtolower(str_replace('__', '.', substr($key, 6)));
+                $container->setParameter($param, $value);
+            }
+        }
+    }
+}
