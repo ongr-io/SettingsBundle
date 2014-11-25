940a83d00b01a333020fe4d3484fc6afb757b613
Gediminas Šaltenis <gediminas.saltenis@ongr.io>
2014-11-25 10:56:31 +0200

    Add encryption twig extension

---
+++ Twig/EncryptionExtension.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\AdminBundle\Twig;
+
+class EncryptionExtension extends \Twig_Extension
+{
+
+    const NAME = 'encryption_extension';
+
+
+    public function getFilters()
+    {
+        return [
+            'base64Encode' => new \Twig_Filter_Method($this, 'base64EncodeFilter'),
+        ];
+    }
+
+    /**
+     * Encodes string to base64
+     *
+     * @param $str
+     * @return string
+     */
+    public function base64EncodeFilter($str)
+    {
+        return base64_encode($str);
+    }
+
+    /**
+     * Decodes string from base64
+     *
+     * @param $str
+     * @return string
+     */
+    public function base64DecodeFilter($str)
+    {
+        return base64_decode($str);
+    }
+
+
+    /**
+     * Get name of the twig extension
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        $name = self::NAME;
+
+        return $name;
+    }
+}
