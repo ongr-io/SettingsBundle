62eb35b0bc617de5ebab37b9f9d3ec9462e54fb2
Gediminas Šaltenis <gediminas.saltenis@ongr.io>
2014-12-08 15:14:20 +0200

    Add Settings Manager Controller Test

---
+++ Tests/Functional/DependencyInjection/DummyService.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\AdminBundle\Tests\Functional\DependencyInjection;
+
+/**
+ * Dummy service for integration test.
+ */
+class DummyService
+{
+    /**
+     * @var String to store setting.
+     */
+    protected $setting;
+
+    /**
+     * Set setting.
+     *
+     * @param string $setting
+     */
+    public function setSetting1($setting)
+    {
+        $this->setting = $setting;
+    }
+
+    /**
+     * Get setting.
+     *
+     * @return string
+     */
+    public function getSetting1()
+    {
+        return $this->setting;
+    }
+}
