066447777dc03b38fad0d923f25bbd78cb5549a1
Gediminas Šaltenis <gediminas.saltenis@ongr.io>
2014-12-08 15:20:06 +0200

    Add provider pass test

---
+++ Tests/Functional/DependencyInjection/Compiler/ProviderPassTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\AdminBundle\Tests\Functional\DependencyInjection\Compiler;
+
+use ONGR\AdminBundle\DependencyInjection\Compiler\ProviderPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+class ProviderPassTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test for process().
+     */
+    public function testProcess()
+    {
+        $container = new ContainerBuilder();
+        $container->setDefinition('ongr_admin.settings_container', new Definition());
+        $container->setParameter('ongr_admin.settings_container.profiles', ['default', 'custom']);
+        $container->setParameter(
+            'ongr_admin.settings_provider.class',
+            'ONGR\\AdminBundle\\Settings\\Common\\Provider\\ManagerAwareSettingProvider.php'
+        );
+
+        $definition = new Definition();
+        $definition->addTag('ongr_admin.ongr_admin', ['profile' => 'custom']);
+        $container->setDefinition('ongr_admin.custom_settings_provider', $definition);
+
+        $definition = new Definition();
+        $definition->addTag('ongr_admin.settings_provider');
+        $container->setDefinition('ongr_admin.unregistered_settings_provider', $definition);
+
+        $pass = new ProviderPass();
+        $pass->process($container);
+
+        $methodCalls = $container->getDefinition('ongr_admin.settings_container')->getMethodCalls();
+        $this->assertCount(3, $methodCalls);
+    }
+}
