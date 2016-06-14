e391d7aba474b3d8ce4e50357558453c4405a9e2
Simonas Šerlinskas <simonas.serlinskas@gmail.com>
2016-06-14 15:04:13 +0300

    do not include node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react']
-                }
+                exclude: /node_modules/
             },
             {test: /\.scss$/, loader: ExtractTextPlugin.extract('css!sass')}
         ]
