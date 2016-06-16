1168fd647a4096bf8b43cce74d717670f5784eef
Kristin Baumann <kristin.baumann@8bitrockr.com>
2016-06-16 17:45:14 +0700

    Loading data from local json file

--- Resources/js/components/SettingsTable.js
+++ Resources/js/components/SettingsTable.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table'; 
 import { Button } from 'react-bootstrap';
+import $ from 'jquery';
 
 
 var actionButtons = (<Button bsStyle="primary" onClick={onClickEdit}>Edit</Button>)
@@ -8,34 +9,6 @@ var actionButtons = (<Button bsStyle="primary" onClick={onClickEdit}>Edit</Butto
 function onClickEdit(){
   console.log("click")
 }
-
-const settings = [
-  {
-      setting_name: "Setting 1",
-      setting_desc: "Desc 1",
-      setting_type: "bool",
-      setting_value: "true",
-      setting_actions: actionButtons 
-  },{
-      setting_name: "Setting 2",
-      setting_desc: "Desc 2",
-      setting_type: "object",
-      setting_value: "object",
-      setting_actions: actionButtons 
-  },{
-      setting_name: "Setting 3",
-      setting_desc: "Desc 3",
-      setting_type: "string",
-      setting_value: "string",
-      setting_actions: actionButtons 
-  },{
-      setting_name: "Setting 4",
-      setting_desc: "Desc 4",
-      setting_type: "array",
-      setting_value: "[1, 3, 5]",
-      setting_actions: actionButtons 
-  }
-];
 
 const setting_types = ['bool', 'string', 'array', 'object']
 
@@ -56,6 +29,7 @@ function arrayFormatter(value, row){
 }
 
 function valueFormatter(value, row){
+  return value.toString();
   switch(row.setting_type){
       case 'bool': return boolFormatter(value, row);
       case 'object': return objectFormatter(value, row);
@@ -65,71 +39,43 @@ function valueFormatter(value, row){
   }
 }   
 
-function onRowSelect(row, isSelected){
-  console.log(row);
-  console.log("selected: " + isSelected)
-}
-
-    
-const selectRowProp = {
-  mode: "radio",
-  clickToSelect: true,
-  bgColor: "rgb(167, 167, 167)",
-  onSelect: onRowSelect
-}
-
-const edit_type = {
-    type: "select",
-    options:{
-        values: setting_types
-    }
-}
-
-const edit_value = {
-    type: "textarea"
-}
-
-function onAfterSaveCell(row, cellName, cellValue) {
-  console.log("Save cell '"+cellName+"' with value '"+cellValue+"'");
-  console.log("Thw whole row :");
-  console.log(row);
-
-  console.log(row.setting_type)
-  switch(row.setting_type){
-      case 'bool': row.setting_value='true'; break;
-      case 'object': row.setting_value='newobject'; break;
-      case 'string': row.setting_value='newstring'; break;
-      case 'array': row.setting_value='newarray'; break;
-      default: row.setting_value='';         
-  }
-}
-
-const cellEditProp = {
-  mode: "click",
-  blurToSave: true,
-  afterSaveCell: onAfterSaveCell
-}
-
 
 class SettingsTable extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  };
+
+  componentDidMount(){
+    $.getJSON("../data.json", function(result) {
+      this.setState({data: this.filterInput(result)});
+    }.bind(this))
+  };
+
+  filterInput(data){
+    return data.documents
+  };
 
   render(){
-      return (
-          <div>
-          <BootstrapTable data={settings} striped={true} hover={true} search={true}>
-              <TableHeaderColumn isKey={true} dataField="setting_name" dataSort={true}>Name</TableHeaderColumn>
-              <TableHeaderColumn dataField="setting_desc" dataSort={true}>Description</TableHeaderColumn>
-              <TableHeaderColumn dataField="setting_type" dataSort={true} editable={edit_type}>Type</TableHeaderColumn>
-              <TableHeaderColumn dataField="setting_value" dataSort={false} dataFormat={valueFormatter} editable={edit_value}>Value</TableHeaderColumn>
-              <TableHeaderColumn dataField="setting_actions">Actions</TableHeaderColumn>
-          </BootstrapTable>
-          </div>
-      )
+      if(this.state.data){
+        return (
+            <BootstrapTable data={this.state.data} striped={true} hover={true} search={true}>
+                <TableHeaderColumn isKey={true} hidden={true} dataField="id">ID</TableHeaderColumn>
+                <TableHeaderColumn dataField="name" dataSort={true}>Name</TableHeaderColumn>
+                <TableHeaderColumn dataField="description" dataSort={true}>Description</TableHeaderColumn>
+                <TableHeaderColumn dataField="type" dataSort={true}>Type</TableHeaderColumn>
+                <TableHeaderColumn dataField="value" dataSort={false} dataFormat={valueFormatter}>Value</TableHeaderColumn>
+            </BootstrapTable>
+        )
+        
+      }else{
+        return (<p>Loading</p>)
+      }
   }
 }
 
 export default SettingsTable
 
-
-
+        
+// <TableHeaderColumn dataField="setting_actions">Actions</TableHeaderColumn>
 
