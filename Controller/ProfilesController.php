128de0cb7b144624f7bc528043ec050516231e2e
Simonas Šerlinskas <simonas.serlinskas@gmail.com>
2016-06-23 15:42:30 +0300

    added full data source for profiles

--- Controller/ProfilesController.php
+++ Controller/ProfilesController.php
@@ -12,7 +12,9 @@
 namespace ONGR\SettingsBundle\Controller;
 
 use ONGR\ElasticsearchBundle\Result\DocumentIterator;
+use ONGR\ElasticsearchBundle\Service\Repository;
 use ONGR\ElasticsearchDSL\Aggregation\TermsAggregation;
+use ONGR\ElasticsearchDSL\Aggregation\TopHitsAggregation;
 use ONGR\ElasticsearchDSL\Search;
 use ONGR\ElasticsearchBundle\Result\Aggregation\AggregationValue;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -62,5 +64,45 @@ class ProfilesController extends Controller
         }
 
         return new JsonResponse($profiles);
+    }
+
+    /**
+     * Returns a json list of profiles
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function getFullProfilesAction(Request $request)
+    {
+        $profiles = [];
+        /** @var Repository $repo */
+        $repo = $this->get($this->getParameter('ongr_settings.repo'));
+
+        $search = $repo->createSearch();
+        $topHitsAgg = new TopHitsAggregation('documents', 100);
+        $termAgg = new TermsAggregation('profiles', 'profile');
+        $termAgg->addAggregation($topHitsAgg);
+        $search->addAggregation($termAgg);
+
+        $result = $repo->execute($search);
+
+        /** @var AggregationValue $agg */
+        foreach ($result->getAggregation('profiles') as $agg) {
+            $settings = [];
+            $docs = $agg->getAggregation('documents');
+            foreach ($docs['hits']['hits'] as $doc) {
+                $settings[] = $doc['_source']['name'];
+            }
+
+            $profiles[] = [
+                'name' => $agg->getValue('key'),
+                'settings' => implode(', ', $settings),
+            ];
+        }
+
+        return new JsonResponse(
+            ['count' => count($profiles), 'documents' => $profiles]
+        );
     }
 }
