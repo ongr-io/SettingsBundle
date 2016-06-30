9f963a12c3a8c8150adac1b92fc2058e56e8d67d
Simonas Šerlinskas <simonas.serlinskas@gmail.com>
2016-06-30 09:52:46 +0300

    setting getter twig extension new prototype

---
+++ Twig/SettingExtension.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\SettingsBundle\Twig;
+use ONGR\SettingsBundle\Document\Setting;
+use ONGR\SettingsBundle\Service\SettingsManager;
+
+/**
+ * Class SettingExtension to show settings value on twig.
+ */
+class SettingExtension extends \Twig_Extension
+{
+    /**
+     * Extension name
+     */
+    const NAME = 'ongr_setting_extension';
+
+    /**
+     * @var SettingsManager
+     */
+    private $manager;
+
+    /**
+     * @param SettingsManager $manager
+     */
+    public function __construct($manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('ongr_setting', [$this, 'getSettingValue']),
+        ];
+    }
+
+    /**
+     * @param string $name
+     * @param bool   $default
+     *
+     * @return mixed
+     */
+    public function getSettingValue($name, $default = false)
+    {
+//        $activeProfilesCookie = $this->get('ongr_settings.cookie.active_profiles');
+
+        /** @var Setting $setting */
+        $setting = $this->manager->get($name);
+
+        if ($setting) {
+            return $setting->getValue();
+        }
+
+        return $default;
+    }
+}
